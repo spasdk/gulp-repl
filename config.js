0b188bca889079b991086e6cebb1f8199d4d456b
Stanislav Kalashnik <sk@infomir.eu>
2016-01-26 19:54:04 +0200

    rework config handling

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /**
- * Configuration for repl gulp task.
+ * Base configuration for repl gulp task.
  *
  * @author Stanislav Kalashnik <darkpark.main@gmail.com>
  * @license GNU GENERAL PUBLIC LICENSE Version 3
@@ -7,15 +7,17 @@
 
 'use strict';
 
-var extend = require('extend'),
-    config = require('spa-gulp/config');
+var extend   = require('extend'),
+    config   = require('spa-gulp/config'),
+    profiles = {};
 
 
-// base config
-// each profile inherits all options from the "default" profile
-module.exports = extend(true, {}, config, {
-    default: {
-        // indicate readiness to accept commands
-        prompt: ''
-    }
+// main
+profiles.default = extend(true, {}, config, {
+    // indicate readiness to accept commands
+    prompt: ''
 });
+
+
+// public
+module.exports = profiles;
